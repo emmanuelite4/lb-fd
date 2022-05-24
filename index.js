d487ac5baf1ec71b8de1249701ab45d77af68a42
Emmanuel <emmanuelite4@gmail.com>
2022-05-24 07:32:31 +0100

    Initialize app with express

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const PORT = process.env.PORT || 3000
+const app = express();
+const router = express.Router()
+
+
+router.get("/workflow", (req, res) => {
+    res.send("Hello world")
+})
+
+
+app.use(router)
+
+
+app.listen(PORT, () => {
+    console.log(`App listening to ${PORT}`)
+})
